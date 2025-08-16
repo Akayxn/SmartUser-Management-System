08c37e08ca58159197a94d9af23d4215886d6da0
Akayxn <yujansharma20621129@gmail.com>
2025-08-16 21:02:47 +0545

    fixed some stuffs for the sidebar and the ui is much cleaner and with some small animatons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
-import Sidebar from "./Components/Sidebar"
-import  "./App.css";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import Sidebar from "./Components/Sidebar";
+import "./App.css";
 
 function App() {
-  return(
-    <div className="App">
-      <Sidebar/>
-    </div>
-  )
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <div className="AppSection">
+        <Sidebar />
+
+          <Routes>
+            <Route path="/dashboard"></Route>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../App.css";
 import { SidebarData } from "./SidebarData";
 
 function Sidebar() {
+  const [Selected, setSelected] = useState(0);
+  const navigate = useNavigate();
+
   return (
     <div className="Sidebar">
       <h1 className="logo">DataHive</h1>
       <ul className="Sidebar-items">
         {SidebarData.map((val, key) => {
           return (
-            <li key={key} onClick={()=>{window.location.pathname = val.link}}>
+            <li
+              key={key}
+              onClick={() => {
+                setSelected(key);
+                navigate(val.link)
+              }}
+              className={Selected === key ? "active" : ""}
+            >
               <div>{val.icon}</div>
               <div>{val.title}</div>
             </li>
