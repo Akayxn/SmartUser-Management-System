f0cbc50cf9788efce143f74efbf09d56b5aebf02
Akayxn <yujansharma20621129@gmail.com>
2025-08-16 21:12:29 +0545

    added a new data for a new row in the sideBarData

--- src/Components/SidebarData.jsx
+++ src/Components/SidebarData.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
-import InfoIcon from '@mui/icons-material/Info';
+import React from "react";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import InfoIcon from "@mui/icons-material/Info";
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
 export const SidebarData = [
-    {
+  {
     title: "Dashboard",
     icon: <DashboardIcon></DashboardIcon>,
-    link: "/dashboard"
-    },
-    {
+    link: "/dashboard",
+  },
+  {
     title: "User List",
     icon: <FormatListBulletedIcon></FormatListBulletedIcon>,
-    link: "/userlist"
-    },
-    {
+    link: "/userlist",
+  },
+  {
+    title: "Add New User",
+    icon: <PersonAddIcon></PersonAddIcon>,
+    link: "/addUser",
+  },
+  {
     title: "About",
-    icon: <InfoIcon/>,
-    link: "/about"
-    }
-]
+    icon: <InfoIcon />,
+    link: "/about",
+  },
+];
